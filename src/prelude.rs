31092a41f04c7d661a4f161e6e153b0ce6124c6e
Ľubomír Kurčák <lubomirkurcak@gmail.com>
2024-03-10 18:57:58 +0100

    more prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,6 @@
-pub use crate::arraynd::CharArray2d;
-pub use crate::geometric_traits::Movement4Directions;
-pub use crate::vector::Vector;
+pub use crate::aabb::{Aabb, Aabb2, Aabb3, Aabb4};
+pub use crate::arraynd::{Array2d, Array3d, ArrayNd, CharArray2d};
+pub use crate::geometric_traits::*;
+pub use crate::line::{LineV2i32, LineVector, LineVectori32};
+pub use crate::linear_index::LinearIndex;
+pub use crate::vector::{V2i32, Vector, V2, V3, V4};
